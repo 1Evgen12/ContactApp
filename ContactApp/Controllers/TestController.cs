d517c638a9fc6b70b659fee512fe67f3ad54b00a
evgeniy <shilov@mail.ru>
2026-03-06 19:58:26 +0500

    Добавил тестовый контроллер

---
+++ ContactApp/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("test")]
+        public string GetHelloWorldText()
+        {
+            return "Hello world!";
+        }
+
+        [HttpGet("hello/{name}")]
+        public string GetGreetingByName(string name)
+        {
+            return $"Привет, {name}!";
+        }
+    }
+}
