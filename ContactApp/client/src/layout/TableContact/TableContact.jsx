fafe9ea017b0b8f3260021e93457a1ec9ee11de9
evgeniy <shilov@mail.ru>
2026-03-09 16:31:08 +0500

    Добавил компоненты для таблицы

---
+++ ContactApp/client/src/layout/TableContact/TableContact.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import RowTableContact from "./components/RowTableContact";
+const TableContact = () => {
+    return (
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Имя контакта</th>
+                    <th>E-mail</th>
+                </tr>
+            </thead>
+            <tbody>
+                <RowTableContact />
+                <RowTableContact />
+                <RowTableContact />
+            </tbody>
+        </table>
+    )
+}
+
+export default TableContact;
---
+++ ContactApp/client/src/layout/TableContact/components/RowTableContact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RowTableContact = () => {
+    return (
+        <tr>
+            <td>1</td>
+            <td>Имя Фамилия 1</td>
+            <td>example@email1.ru</td>
+            <td>Номер телефона 1</td>
+            <td>адрес 1</td>
+        </tr>
+    );
+}
+
+export default RowTableContact;
