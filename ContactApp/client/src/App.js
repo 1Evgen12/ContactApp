0b1db44da7114cf210da352a26c1ac7b389a04d8
evgeniy <shilov@mail.ru>
2026-03-09 19:05:07 +0500

    Кнопка для добавления контакта

--- ContactApp/client/src/App.js
+++ ContactApp/client/src/App.js
@@ -1,12 +1,25 @@
 import TableContact from "./layout/TableContact/TableContact";
-
-const contacts = [
-  { id: 1, name: "Имя Фамилия 1", email: "q@e1.rt", phone: "phone 1", address: "address 1" },
-  { id: 2, name: "Имя Фамилия 2", email: "q@e2.rt", phone: "phone 2", address: "address 2" },
-  { id: 3, name: "Имя Фамилия 3", email: "q@e3.rt", phone: "phone 3", address: "address 3" },
-]
+import React, { useState } from "react";
 
 const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 1, name: "Имя Фамилия 1", email: "q@e1.rt", phone: "phone 1", address: "address 1" },
+    { id: 2, name: "Имя Фамилия 2", email: "q@e2.rt", phone: "phone 2", address: "address 2" },
+    { id: 3, name: "Имя Фамилия 3", email: "q@e3.rt", phone: "phone 3", address: "address 3" },
+  ])
+
+  const addContact = () => {
+    const Id = Math.max(...contacts.map(e => e.id)) + 1;
+    const item = {
+      id: Id,
+      name: "Имя Фамилия 1",
+      email: "q@e1.rt",
+      phone: "phone 1",
+      address: "address 1"
+    }
+    setContacts([...contacts, item]);
+  }
+
   return (
     <div className="container mt-5">
       <div className="card">
@@ -16,6 +29,14 @@ const App = () => {
 
         <div className="card-body">
           <TableContact contacts={contacts} />
+          <div>
+            <button
+              className="btn btn-primary"
+              onClick={() => addContact()}
+            >
+              Добавить контакт
+            </button>
+          </div>
         </div>
       </div>
     </div>
